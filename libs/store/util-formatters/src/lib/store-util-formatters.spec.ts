d882ee6af0cdb231efc3841ae272e22b1cecc14c
Emily M Klassen <forivall@gmail.com>
2022-10-18 13:27:39 -0700

    fix tests more

--- libs/store/util-formatters/src/lib/store-util-formatters.spec.ts
+++ libs/store/util-formatters/src/lib/store-util-formatters.spec.ts
@@ -1,7 +1,7 @@
-import { storeUtilFormatters } from './store-util-formatters';
+import { formatRating } from './store-util-formatters';
 
-describe('storeUtilFormatters', () => {
+describe('formatRating', () => {
   it('should work', () => {
-    expect(storeUtilFormatters()).toEqual('store-util-formatters');
+    expect(formatRating()).toEqual('0 / 10');
   });
 });
