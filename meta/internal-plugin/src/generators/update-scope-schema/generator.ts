69b7e6cf522f73f6ef5f91fbe9a6750d0fdb413b
Emily M Klassen <forivall@gmail.com>
2022-10-18 11:58:27 -0700

    lab14

--- meta/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ meta/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,69 +1,46 @@
 import {
   addProjectConfiguration,
   formatFiles,
-  generateFiles,
-  getWorkspaceLayout,
-  names,
-  offsetFromRoot,
+  getProjects,
+  ProjectConfiguration,
   Tree,
+  updateJson,
 } from '@nrwl/devkit';
-import * as path from 'path';
 import { UpdateScopeSchemaGeneratorSchema } from './schema';
 
-interface NormalizedSchema extends UpdateScopeSchemaGeneratorSchema {
-  projectName: string;
-  projectRoot: string;
-  projectDirectory: string;
-  parsedTags: string[];
+function addScopeIfMissing() {}
+
+const scopePrefix = 'scope:';
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const projects: ProjectConfiguration[] = Array.from(projectMap.values());
+  const allScopes: string[] = projects
+    .map((project) =>
+      (project.tags ?? []).filter((tag) => tag.startsWith(scopePrefix))
+    )
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope) => scope.slice(scopePrefix.length));
+  return Array.from(new Set(allScopes));
 }
 
-function normalizeOptions(tree: Tree, options: UpdateScopeSchemaGeneratorSchema): NormalizedSchema {
-  const name = names(options.name).fileName;
-  const projectDirectory = options.directory
-    ? `${names(options.directory).fileName}/${name}`
-    : name;
-  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
-  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
-  const parsedTags = options.tags
-    ? options.tags.split(',').map((s) => s.trim())
-    : [];
-
-  return {
-    ...options,
-    projectName,
-    projectRoot,
-    projectDirectory,
-    parsedTags,
-  };
+export default async function (
+  tree: Tree,
+  options: UpdateScopeSchemaGeneratorSchema
+) {
+  updateJson(tree, 'nx.json', (value) => {
+    value.defaultProject = 'api';
+    return value;
+  });
+  const scopes = getScopes(getProjects(tree));
+  const scopesType = scopes.map((s) => `'${s}'`).join(' | ');
+  const content = /*ts*/ `
+export interface UtilLibGeneratorSchema {
+  name: string;
+  directory: ${scopesType};
 }
-
-function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
-}
-
-export default async function (tree: Tree, options: UpdateScopeSchemaGeneratorSchema) {
-  const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(
-    tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@bg-hoard/internal-plugin:build",
-        },
-      },
-      tags: normalizedOptions.parsedTags,
-    }
+`.trimStart();
+  tree.write(
+    'meta/internal-plugin/src/generators/util-lib/schema.d.ts',
+    content
   );
-  addFiles(tree, normalizedOptions);
   await formatFiles(tree);
 }
--- meta/internal-plugin/src/generators/update-scope-schema/schema.d.ts
+++ meta/internal-plugin/src/generators/update-scope-schema/schema.d.ts
@@ -1,5 +1,5 @@
 export interface UpdateScopeSchemaGeneratorSchema {
-    name: string;
+    name?: string;
     tags?: string;
     directory?: string;
 }
