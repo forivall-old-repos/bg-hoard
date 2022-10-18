f501f78a3292aaff1305b5e40b9650c10c14745d
Emily M Klassen <forivall@gmail.com>
2022-10-18 13:10:15 -0700

    test: add missing httpclienttestingmodule

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
